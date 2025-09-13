059292651128d61d3311bd6fb658df22820f7506
LAPTOP-4K6GB0DK\Marla Bitran <marlabitran@dolphyns.local>
2025-09-13 16:04:44 +0300

    Migration from non controlled source

---
+++ GameStore.Frontend/Program.cs
@@ -0,0 +1,19 @@
+using GameStore.Frontend.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>();
+
+app.Run();
